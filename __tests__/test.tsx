e6e9e1f3589c2dd8dcbdbae470d8e1e61db0ce67
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-28 20:07:35 +0900

    Add test for nested component

--- __tests__/test.tsx
+++ __tests__/test.tsx
@@ -2,14 +2,31 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import FindElement from ".."
 
-class Component extends FindElement<{}, {}> {
+class FindElementComponent extends FindElement<{}, {}> {
 }
 
 test("FindElement finds child DOM element", () => {
   const root = document.createElement("div")
-  const view = <Component>
+  const view = <FindElementComponent>
     <div id="elem" />
-  </Component>
-  const component = ReactDOM.render(view, root) as Component
+  </FindElementComponent>
+  const findElement = ReactDOM.render(view, root) as FindElementComponent
+  expect(findElement.element.id).toBe("elem")
+})
+
+class Foo extends React.Component<{}, {}> {
+  render() {
+    return React.Children.only(this.props.children)
+  }
+}
+
+test("FindElement finds descendant DOM element nested in other components", () => {
+  const root = document.createElement("div")
+  const view = <FindElementComponent>
+    <Foo>
+      <div id="elem" />
+    </Foo>
+  </FindElementComponent>
+  const component = ReactDOM.render(view, root) as FindElementComponent
   expect(component.element.id).toBe("elem")
 })
